e7099e637c1395198c677c433edb02f2d374e323
kon <sound.recording.man@gmail.com>
2016-02-23 13:16:59 +0900

    カメラの位置（ズーム）によって回転するときに感度を調整する処理を追加 拡大しているときはゆっくり、球のときは早く回転する

--- Assets/RotateSphere.cs
+++ Assets/RotateSphere.cs
@@ -3,9 +3,10 @@ using System.Collections;
 
 public class RotateSphere : MonoBehaviour {
 
-	private const float MOUSE_SENSITIVITY = 0.2f;
+	private const float MOUSE_SENSITIVITY = 0.1f;
 	private Quaternion defaultRotation;
 	private Vector3 prevMousePos;
+	private Vector3 prevScrollSpeed;
 
 	// Use this for initialization
 	void Start () {
@@ -23,10 +24,21 @@ public class RotateSphere : MonoBehaviour {
 		// マウスの左押下開始で初期位置を取得する
 		if (Input.GetMouseButtonDown (0)) {
 			prevMousePos = Input.mousePosition;
+			prevScrollSpeed = Vector3.zero;
 		}
 
-		// マウスの左ボタン未押下では以下の処理は行わない
-		if (!Input.GetMouseButton(0)) return;
+		// カメラ位置によって感度を調整する
+		float cameraZoom = Camera.main.transform.position.z;
+		float sensitivity = MOUSE_SENSITIVITY + (-1 * cameraZoom * 0.1f);
+
+		// マウスの左ボタン未押下では慣性で回す
+		if (!Input.GetMouseButton (0)) {
+			prevScrollSpeed *= 0.95f;
+			//Debug.Log (prevScrollSpeed);
+			transform.Rotate(0, prevScrollSpeed.x * sensitivity, 0, Space.Self);
+			transform.Rotate (-1 * prevScrollSpeed.y * sensitivity, 0, 0, Space.World);
+			return;
+		}
 
 		// 以下はドラッグ中.マウスの移動量を計算する
 		Vector3 curMousePos = Input.mousePosition;
@@ -34,10 +46,12 @@ public class RotateSphere : MonoBehaviour {
 		prevMousePos = curMousePos;
 
 		// マウスが移動していなければ以下の処理は行わない
-		if (mouseMoveVol == Vector3.zero) return;
+		if (mouseMoveVol != Vector3.zero) {
+			// 回転する
+			transform.Rotate (0, mouseMoveVol.x * sensitivity, 0, Space.Self);
+			transform.Rotate (-1 * mouseMoveVol.y * sensitivity, 0, 0, Space.World);
+		}
 
-		// 回転する
-		transform.Rotate(0, mouseMoveVol.x * MOUSE_SENSITIVITY, 0, Space.Self);
-		transform.Rotate (-1 * mouseMoveVol.y * MOUSE_SENSITIVITY, 0, 0, Space.World);
+		prevScrollSpeed = mouseMoveVol;
 	}
 }
