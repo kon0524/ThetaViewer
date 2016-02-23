39d547f3678f86fe990e7e68d23afd149c21397c
kon <sound.recording.man@gmail.com>
2016-02-23 10:19:01 +0900

    カメラズームを実装

--- Assets/RotateSphere.cs
+++ Assets/RotateSphere.cs
@@ -3,13 +3,42 @@ using System.Collections;
 
 public class RotateSphere : MonoBehaviour {
 
+	private const float MOUSE_SENSITIVITY = 0.2f;
+	private Quaternion defaultRotation;
+	private Vector3 prevMousePos;
+
 	// Use this for initialization
 	void Start () {
-	
+		defaultRotation = transform.rotation;
+		Debug.Log ("defaultRotation : " + defaultRotation);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Debug.Log ("Update");
+		// スペースキーで初期位置に戻る
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			transform.rotation = defaultRotation;
+		}
+
+		// マウスの左押下開始で初期位置を取得する
+		if (Input.GetMouseButtonDown (0)) {
+			prevMousePos = Input.mousePosition;
+		}
+
+		// マウスの左ボタン未押下では以下の処理は行わない
+		if (!Input.GetMouseButton(0)) return;
+
+		// 以下はドラッグ中.マウスの移動量を計算する
+		Vector3 curMousePos = Input.mousePosition;
+		Vector3 mouseMoveVol = curMousePos - prevMousePos;
+		prevMousePos = curMousePos;
+
+		// マウスが移動していなければ以下の処理は行わない
+		if (mouseMoveVol == Vector3.zero) return;
+
+		// Y軸を回転する
+		transform.Rotate(0, mouseMoveVol.x * MOUSE_SENSITIVITY, 0);
+		Debug.Log("mouseMoveVol : " + mouseMoveVol);
+
 	}
 }
---
+++ Assets/ZoomCamera.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZoomCamera : MonoBehaviour {
+
+	private const float ZOOM_SENSITIVITY = 0.8f;
+	private const float ZOOM_MAX = 0.8f;
+	private Vector3 defaultPosition;
+
+	// Use this for initialization
+	void Start () {
+		defaultPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// スペースキーで初期位置に戻る
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			transform.position = defaultPosition;
+		}
+
+		// カメラの現在位置とマウスホイールの値
+		Vector3 curPosition = transform.position;
+		float zoom = Input.GetAxis("Mouse ScrollWheel") * ZOOM_SENSITIVITY;
+		if (zoom == 0) return;
+		Debug.Log ("pos : " + curPosition + " zoom : " + zoom);
+
+		// ズームしすぎを判定する
+		if (zoom > 0 && curPosition.z + zoom > ZOOM_MAX) return;
+
+		// ズーム
+		transform.Translate (new Vector3(0, 0, zoom));
+	}
+}
